5eeac6aab8b3da64f25493875c12852022a1df4a
Michael Dahlquist <mwdahl22@g.holycross.edu>
2020-04-13 22:23:45 -0400

    reorganized,added initalize, added if git

--- main.lua
+++ main.lua
@@ -9,32 +9,27 @@ Purpose:
 --Add functions:
 dofile("functions.lua")
 
---Compile java files used
-javac = function (x) return os.execute("javac "..x..".java") end --Annonymous function
-javac("unsplash")
+--Add java file names to compile
+java = {}
+java[1] = "unsplash"
 
---START MAIN
-io.write("Please input the .txt file: ")
-input_text = io.read("*line")
--- TO DO: Add check to see if this file is in the currect directory, and last 4 == ".txt"
-_table = get_lines(input_text)
+--Get table of phrases, the name of the text file where the phrases were, 
+_table, do_git, folder = initalize(java) -- AN EXAMPLE OF MULTIPLE RETURN VALUES!
+
 all_sources(_table)
---Move into a folder date_time/
-
-folder =    string.gsub(
-                string.gsub(
-                    string.gsub(
-                        string.sub(os.date("%x"),1,8).." "..string.sub(os.date("%c"),12,19), 
-                    ":", "_"), 
-                " ", "_"), 
-            "/",  "_")
-
-os.execute("mkdir "..folder)
-os.execute("cp -r *.jpg "..folder.."/")
-os.execute("rm -r *.jpg")
 
 _2md = function (x) return string.sub(x,1,-4).."md" end --Another annonymous function
 
-write_md(_2md(input_text),folder,_table)
-os.execute("cp  ".._2md(input_text).." "..folder.."/")
-os.execute("rm ".._2md(input_text))
+write_md("README.md",folder,_table)
+
+--Move files into the folder, which has the data and time of the start of the execution.
+os.execute("mkdir "..folder)
+os.execute("cp -r *.jpg "..folder.."/")
+os.execute("rm -r *.jpg")
+os.execute("cp  ".."README.md".." "..folder.."/README.md")
+os.execute("rm ".."README.md")
+
+if do_git then 
+    print("Sorry, I have not yet configured the GitHub portion...")
+    --github(folder)
+end
