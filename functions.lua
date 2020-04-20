b204f82f7912fa705b5600d362b009870a66f8bc
Michael Dahlquist <mwdahl22@g.holycross.edu>
2020-04-19 20:10:41 -0400

    4_18 progress

--- functions.lua
+++ functions.lua
@@ -3,87 +3,13 @@ Names: Michael Dahlquist & Kristen Qako
 Class: csci324 - spring2020
 File:  functions.lua
 Purpose:
+    Contains the functions for main.lua
 
 ]]
-function get_lines(file_name)
-    --get_lines(file_name) returns a table of all the lines in file_name
-    --does not include return characters
-        new_table = {}
-        count = 0
-        local file = io.open(file_name, "rb")
-        for line in io.lines(file_name) do
-            count = count + 1
-            --fixes formatting issues
-            line1 = line
-            line1 = string.gsub(line1, "%A", "*")
-            if string.sub(line1,string.len(line1),string.len(line1)) == "*" then
-                line = string.sub(line,1,string.len(line1)-1)
-            end
-            new_table[count] = line
-            --new_table[count] = string.sub(line, 1, string.len(line))
-        end
-        file:close()
-        return new_table
-    --A portion of this code was inspired by:
-    --https://stackoverflow.com/questions/11201262/how-to-read-data-from-a-file-in-lua
-end
-
-function concatenate(new_table, char) 
-    --file_table = ""
-    for i = 1, #new_table do
-        new_table[i] = string.gsub(new_table[i],"%A", char)
-        --file_table[i] = prefix..new_table[i]..".jpg"
-    end
-end
-
-get_unsplash_jpgs = coroutine.create(function (phrases)
-    unsplash_table = phrases
-    concatenate(unsplash_table, ",")
-    java_unsplash = function (x) return os.execute("java unsplash "..x) end -- ANNONYMOUS FUNCTION
-    for i = 1, #phrases do
-        java_unsplash(unsplash_table[i])
-        print("Executed: "..unsplash_table[i])
-        coroutine.yield()
-    end
-end)
-
-secondary_table = coroutine.create(function (phrases)
-    for i = 1, #phrases do
-        print("Executed: "..i/#phrases)
-        coroutine.yield()     
-    end
-end)
-
-function all_sources(phrases)
-    while coroutine.resume(get_unsplash_jpgs, phrases) and coroutine.resume(secondary_table,phrases) do end
-    --[[
-    for i = 1, #phrases do
-        coroutine.resume(get_unsplash_jpgs,phrases)
-        coroutine.resume(secondary_table,phrases)
-    end  
-    ]]
-end
-
-function write_md(file_name,new_table)
-    --Input file names
-    unsplash_table = new_table
-    concatenate(unsplash_table, ",")
-    --Write md
-    local file = io.open(file_name, "w") --open file
-    io.output(file) --set output to file
-
-    io.write("# "..string.sub(file_name,1,-5)..'\n'..'\n') --Write title
-    
-    for i = 1, #new_table do
-        io.write("**"..new_table[i].."**"..'\n\n')
-        io.write("!["..new_table[i].."](".."unsplash_"..unsplash_table[i]..".jpg)\n\n")
-    end
-    
-    file:close() --close file
-end
 
 function initalize (java)
     folder_name = string.gsub(string.gsub(string.sub(os.date("%x"),1,8).."_"..string.sub(os.date("%c"),12,19), ":", "_"), "/",  "_")
+    --folder_name = "test_git_05"
 
     --Compile all java files used
     javac = function (x) return os.execute("javac "..x..".java") end --Annonymous function
@@ -109,16 +35,95 @@ function initalize (java)
         end
     end
     a_table = get_lines(txt_file)
-    git = true
     return a_table, txt_file, folder_name, git
-
-
 end
 
+function get_lines(file_name)
+    --get_lines(file_name) returns a table of all the lines in file_name
+    --does not include return characters
+        new_table = {}
+        local file = io.open(file_name, "rb")
+        for line in io.lines(file_name) do
+            --fixes formatting issues
+            line1 = line
+            line1 = string.gsub(line1, "%A", "*")
+            if string.sub(line1,string.len(line1),string.len(line1)) == "*" then
+                line = string.sub(line,1,string.len(line1)-1)
+            end
+            new_table[#new_table+1] = line
+        end
+        file:close()
+        return new_table
+    --A portion of this code was inspired by:
+    --https://stackoverflow.com/questions/11201262/how-to-read-data-from-a-file-in-lua
+end
+
+get_unsplash_jpgs = coroutine.create(function (phrases, time)
+    --coroutine that downloads one image from unsplash each time it is resumed
+    java_unsplash = function (x) return os.execute("java unsplash "..x) end -- ANNONYMOUS FUNCTION
+    for i = 1, #phrases do
+        time["unsplash"][i] = os.clock()
+        java_unsplash(phrases[i])
+        print("Executed unsplash: "..phrases[i])
+        time["unsplash"][i] = os.clock() - time["unsplash"][i]
+        coroutine.yield()
+    end
+end)
+
+get_loremflickr = coroutine.create(function (phrases,time)
+    --coroutine that downloads one image from loremflickr each time it is resumed
+    java_loremflickr = function (x) return os.execute("java loremflickr "..x) end -- ANNONYMOUS FUNCTION
+    for i = 1, #phrases do
+        time["loremflickr"][i] = os.clock()
+        java_loremflickr(phrases[i])
+        print("Executed loremflickr: "..phrases[i])
+        time["loremflickr"][i] = os.clock() - time["loremflickr"][i]
+        coroutine.yield()
+    end
+end)
+
+function all_sources(phrases)
+    if #phrases == 0 then return end --if there are no images, don't execute
+    count = 1
+    phrases[count] = string.gsub(phrases[count],"%A", ",")
+    time = {}
+    time["unsplash"] = {}
+    time["loremflickr"] = {}
+    while coroutine.resume(get_unsplash_jpgs, phrases, time) and coroutine.resume(get_loremflickr,phrases,time) do
+        if count < #phrases then
+            count = count + 1
+            phrases[count] = string.gsub(phrases[count],"%A", ",")
+        end
+    end
+    return time
+end
+
+function write_md(file_name,new_table,time)
+    --Write md
+    local file = io.open(file_name, "w") --open file
+    io.output(file) --set output to file
+
+    io.write("# "..string.sub(file_name,1,-4)..'\n'..'\n') --Write title
+    
+    for i = 1, #new_table do
+        if time["unsplash"][i] <= time["loremflickr"][i] then
+            io.write("**"..new_table[i].."**"..'\n\n')
+            io.write("!["..new_table[i].."](".."unsplash_"..new_table[i]..".jpg)\n\n")
+        else
+            io.write("**"..new_table[i].."**"..'\n\n')
+            io.write("!["..new_table[i].."](".."loremflickr_"..new_table[i]..".jpg)\n\n")
+        end
+    end
+    
+    file:close() --close file
+end
 
 function github (folder)
-    os.execute("git add "..folder.."/\\*.txt") --"git add Documentation/\*.txt"
+    os.execute("git add "..folder.."/\\*.jpg") --"git add Documentation/\*.txt"
     os.execute("git add "..folder.."/\\*.md")
     os.execute("git commit -m \""..folder.."\"")
     os.execute("git push")
+    print("Please note that the site might not be rendered for a while")
+    print("Here is the link:")
+    print("https://www.michaeldahlquist.github.io/test-to-markdown-images/"..folder.."/")
 end
--- main.lua
+++ main.lua
@@ -6,32 +6,29 @@ Purpose:
 
 ]]
 
---Add functions:
+--Define functions:
 dofile("functions.lua")
 
 --Add java file names to compile
 java = {}
 java[1] = "unsplash"
+java[2] = "loremflickr"
 
 --Get table of phrases, the name of the text file where the phrases were, 
 _table, txt_file, folder, do_git = initalize(java) -- AN EXAMPLE OF MULTIPLE RETURN VALUES!
 
-all_sources(_table)
+time = all_sources(_table) --Downloads pictures and returns download time tables
 
 _2md = function (x) return string.sub(x,1,-4).."md" end --Another annonymous function
 
-write_md(_2md(txt_file),_table)
+write_md(_2md(txt_file),_table,time) --write md file of fastest downloaded image of each word
 
 --Move files into the folder, which has the data and time of the start of the execution.
-
 os.execute("mkdir "..folder)
-os.execute("cp -r *.jpg "..folder.."/")
-os.execute("rm -r *.jpg")
-os.execute("cp  ".._2md(txt_file).." "..folder.."/README.md")
-os.execute("rm ".._2md(txt_file))
+os.execute("mv *.jpg "..folder.."/")
+os.execute("mv ".._2md(txt_file).." "..folder.."/README.md")
 
-
+--If user wishes to send markdown files to GitHub they may.
 if do_git then 
-    print("Sorry, I have not yet configured the GitHub portion...")
-    --github(folder)
+    github(folder)
 end
